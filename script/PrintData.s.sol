67ed1a1dc6678054deeb2e558d45ec7b53b82e21
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-03-18 01:45:35 -0400

    docs: natspec data printing script

--- script/PrintData.s.sol
+++ script/PrintData.s.sol
@@ -6,11 +6,14 @@ import {console} from "forge-std/Test.sol";
 
 import {FiveFiveFiveData} from "src/utils/FiveFiveFiveData.sol";
 
+/// @notice A testing script to use during development to print out the mileage
+/// and location data for each run from {FiveFiveFiveData}.
 contract PrintDataScript is Script {
     // -------------------------------------------------------------------------
     // Script `run()`
     // -------------------------------------------------------------------------
 
+    /// @notice Calls {FiveFiveFiveArt} and retrieves data for a range of days.
     function run() public view {
         for (uint256 i = 325; i < 336; ) {
             uint256 mileage = FiveFiveFiveData.getDayMileage(i);
